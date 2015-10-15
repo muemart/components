07eb3259d42f576d931ed2911f690919b36ba091
Martin Müller <muemart@student.ethz.ch>
2015-10-15 19:43:54 +0200

    Add a simple service to retrieve traceroute results

--- ripe-atlas-v2/ripe-atlas-v2.py
+++ ripe-atlas-v2/ripe-atlas-v2.py
@@ -8,6 +8,7 @@ import pytz
 def services():
     services = []
     services.append(AtlasResultService(result_common_cap(result_ping_cap())))
+    services.append(AtlasResultService(result_common_cap(result_trace_cap())))
     return services
 
 def result_ping_cap():
@@ -17,6 +18,14 @@ def result_ping_cap():
     cap.add_result_column("delay.twoway.icmp.us.50pct")
     cap.add_result_column("delay.twoway.icmp.us.max")
     cap.add_result_column("delay.twoway.icmp.count")
+    return cap
+
+def result_trace_cap():
+    cap = mplane.model.Capability(verb = mplane.model.VERB_QUERY, label = "ripeatlas-trace-result", when = "past ... now")
+    cap.add_result_column("intermediate.ip4")
+    cap.add_result_column("ripeatlas.traceroute_id")
+    cap.add_result_column("hops.ip")
+    cap.add_result_column("rtt.ms")
     return cap
 
 def result_common_cap(cap):
@@ -38,13 +47,13 @@ class AtlasResultService(mplane.scheduler.Service):
             "start": starttime,
             "end": endtime
         }
+        is_success, reqanswer = cousteau.AtlasResultsRequest(**kwargs).create();
+        if not is_success:
+            raise RuntimeError("AtlasResultsRequest was not successful")
         res = mplane.model.Result(specification=spec)
+        measstart = datetime.now(tz=pytz.UTC)
+        measend = datetime.fromtimestamp(0, pytz.UTC)
         if  "ripeatlas-ping-result" in spec.get_label():
-            is_success, reqanswer = cousteau.AtlasResultsRequest(**kwargs).create();
-            if not is_success:
-                raise RuntimeError("AtlasResultsRequest was not successful")
-            measstart = datetime.now(tz=pytz.UTC)
-            measend = datetime.fromtimestamp(0, pytz.UTC)
             for i, proberes in enumerate(reqanswer):
                 result = sagan.PingResult(proberes)
                 res.set_result_value("delay.twoway.icmp.us.min", result.rtt_min * 1000, i)
@@ -58,9 +67,26 @@ class AtlasResultService(mplane.scheduler.Service):
                 res.set_result_value("time", mplane.model.When(a=result.created), i)
                 measstart = result.created if result.created < measstart else measstart
                 measend = result.created if result.created > measend else measend
-            res.set_when(mplane.model.When(a = measstart, b = measend))
-        elif spec.get_label() == "":
-            pass
+        elif "ripeatlas-trace-result" in spec.get_label():
+            i = 0
+            for proberes in reqanswer:
+                result = sagan.TracerouteResult(proberes)
+                for hop in result.hops:
+                    if hop.packets[0].origin == "" or result.origin == "":
+                        continue
+                    #Just take the IP address of the first packet for now
+                    res.set_result_value("intermediate.ip4", hop.packets[0].origin,i)
+                    res.set_result_value("ripeatlas.traceroute_id", hash(str(result.created) + result.origin),i)
+                    res.set_result_value("hops.ip",result.total_hops,i)
+                    res.set_result_value("rtt.ms",result.last_median_rtt,i)
+                    res.set_result_value("source.ip4",result.origin,i)
+                    res.set_result_value("destination.ip4",result.destination_address,i)
+                    res.set_result_value("ripeatlas.probe_id", result.probe_id, i)
+                    res.set_result_value("time", mplane.model.When(a=result.created), i)
+                    i += 1
+                measstart = result.created if result.created < measstart else measstart
+                measend = result.created if result.created > measend else measend
         else:
             raise ValueError("Unknown specification label: " + spec.get_label())
+        res.set_when(mplane.model.When(a = measstart, b = measend))
         return res
