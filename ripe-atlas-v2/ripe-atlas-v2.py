faaaa995cc9418b4caba49e79633db034fac9cfd
Martin Müller <muemart@student.ethz.ch>
2015-11-05 14:16:18 +0100

    Various improvements: - handle values that are None - return all packets for all hops for traceroutes

--- ripe-atlas-v2/ripe-atlas-v2.py
+++ ripe-atlas-v2/ripe-atlas-v2.py
@@ -28,6 +28,7 @@ def result_trace_cap():
     cap = mplane.model.Capability(verb = mplane.model.VERB_MEASURE, label = "ripeatlas-trace-result", when = "past ... future")
     cap.add_result_column("intermediate.ip4")
     cap.add_result_column("ripeatlas.traceroute_id")
+    cap.add_result_column("ripeatlas.hop_index")
     cap.add_result_column("hops.ip")
     cap.add_result_column("rtt.ms")
     return cap
@@ -54,6 +55,12 @@ class AtlasResultService(mplane.scheduler.Service):
     def __init__(self, capability):
         return super().__init__(capability)
 
+    def _notnone(self, value, default):
+        if value is not None:
+            return value
+        else:
+            return default
+
     def run(self, spec, check_interrupt):
         starttime, endtime = spec.when().datetimes()
         msm_id = spec.get_parameter_value("ripeatlas.msm_id")
@@ -69,46 +76,66 @@ class AtlasResultService(mplane.scheduler.Service):
         res = mplane.model.Result(specification=spec)
         measstart = datetime.now(tz=pytz.UTC)
         measend = datetime.fromtimestamp(0, pytz.UTC)
+
+        #
+        #       PING MEASUREMENT
+        #
         if  "ripeatlas-ping-result" in spec.get_label():
             if not cousteau.Measurement(id=msm_id).type == "PING":
                 raise ValueError("Measurement " + str(msm_id) + " ist not of type ping")
-            for i, proberes in enumerate(reqanswer):
+            i = 0
+            for proberes in reqanswer:
                 result = sagan.PingResult(proberes)
-                res.set_result_value("delay.twoway.icmp.us.min", result.rtt_min * 1000, i)
-                res.set_result_value("delay.twoway.icmp.us.mean", result.rtt_average * 1000, i)
-                res.set_result_value("delay.twoway.icmp.us.50pct", result.rtt_median * 1000, i)
-                res.set_result_value("delay.twoway.icmp.us.max", result.rtt_max * 1000, i)
-                res.set_result_value("delay.twoway.icmp.count", result.packets_received, i)
-                res.set_result_value("source.ip4",result.origin,i)
-                res.set_result_value("destination.ip4",result.destination_address,i)
+                if result.is_malformed or result.is_error:
+                   continue
+                res.set_result_value("delay.twoway.icmp.us.min", self._notnone(result.rtt_min, 0) * 1000, i)
+                res.set_result_value("delay.twoway.icmp.us.mean", self._notnone(result.rtt_average, 0) * 1000, i)
+                res.set_result_value("delay.twoway.icmp.us.50pct", self._notnone(result.rtt_median, 0) * 1000, i)
+                res.set_result_value("delay.twoway.icmp.us.max", self._notnone(result.rtt_max, 0) * 1000, i)
+                res.set_result_value("delay.twoway.icmp.count", self._notnone(result.packets_received, 0), i)
+                res.set_result_value("source.ip4", self._notnone(result.origin, "0.0.0.0"), i)
+                res.set_result_value("destination.ip4",self._notnone(result.destination_address, "0.0.0.0"), i)
                 res.set_result_value("ripeatlas.probe_id", result.probe_id, i)
                 res.set_result_value("time", mplane.model.When(a=result.created), i)
                 measstart = result.created if result.created < measstart else measstart
                 measend = result.created if result.created > measend else measend
+                i += 1
+
+        #
+        #       TRACEROUTE MEASUREMENT
+        #
         elif "ripeatlas-trace-result" in spec.get_label():
             if not cousteau.Measurement(id=msm_id).type == "TRACEROUTE":
                 raise ValueError("Measurement " + str(msm_id) + " ist not of type traceroute")
             i = 0
             for proberes in reqanswer:
                 result = sagan.TracerouteResult(proberes)
-                for hop in result.hops:
-                    if hop.packets[0].origin == "" or result.origin == "":
+                if result.is_malformed or result.is_error:
+                    continue
+                for hopindex, hop in enumerate(result.hops):
+                    hopindex = 0
+                    if hop.is_malformed or hop.is_error:
                         continue
-                    #Just take the IP address of the first packet for now
-                    res.set_result_value("intermediate.ip4", hop.packets[0].origin,i)
-                    res.set_result_value("ripeatlas.traceroute_id", hash(str(result.created) + result.origin),i)
-                    res.set_result_value("hops.ip",result.total_hops,i)
-                    res.set_result_value("rtt.ms",result.last_median_rtt,i)
-                    res.set_result_value("source.ip4",result.origin,i)
-                    res.set_result_value("destination.ip4",result.destination_address,i)
-                    res.set_result_value("ripeatlas.probe_id", result.probe_id, i)
-                    res.set_result_value("time", mplane.model.When(a=result.created), i)
-                    i += 1
+                    for packet in hop.packets:
+                        if packet.is_malformed or packet.is_error:
+                           continue
+                        res.set_result_value("intermediate.ip4", self._notnone(packet.origin, "0.0.0.0"),i)
+                        res.set_result_value("ripeatlas.traceroute_id", hash(str(result.created) + result.origin),i)
+                        res.set_result_value("ripeatlas.hop_index", hopindex)
+                        res.set_result_value("hops.ip", self._notnone(result.total_hops, 0), i)
+                        res.set_result_value("rtt.ms", self._notnone(packet.rtt, 0), i)
+                        res.set_result_value("source.ip4", self._notnone(result.origin, "0.0.0.0"), i)
+                        res.set_result_value("destination.ip4", self._notnone(result.destination_address, "0.0.0.0"), i)
+                        res.set_result_value("ripeatlas.probe_id", result.probe_id, i)
+                        res.set_result_value("time", mplane.model.When(a=result.created), i)
+                        i += 1
                 measstart = result.created if result.created < measstart else measstart
                 measend = result.created if result.created > measend else measend
         else:
             raise ValueError("Unknown specification label: " + spec.get_label())
+
         res.set_when(mplane.model.When(a = measstart, b = measend))
+        print("Sending " + str(res.count_result_rows()) + " result rows. This might take a while.")
         return res
 
 class AtlasCreateService(mplane.scheduler.Service):
